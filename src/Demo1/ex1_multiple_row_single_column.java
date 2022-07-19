d308d32bf408336bbcc7f2776fe2bf0d3e838031
user <user@lenovo>
2022-07-19 20:43:38 +0530

    [anuja]:adding ex1

---
+++ src/Demo1/ex1_multiple_row_single_column.java
@@ -0,0 +1,33 @@
+package Demo1;
+// ex1 *..................println to move in next row
+//     *
+//     *
+//     *
+public class ex1_multiple_row_single_column {
+
+	public static void main(String[] args) {
+//		for(int i=1;i<=4;i++)
+//		{
+//			System.out.println("*");
+//		}
+		
+	//Ex2
+		
+	for(int i=1;i<=5;i++)
+	{
+		System.out.println("*");
+	}
+		
+		
+		
+		
+		
+}
+}
+
+
+
+
+
+
+	// use println to move on next row
